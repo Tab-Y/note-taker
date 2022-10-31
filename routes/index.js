87148371e283b44b01a2be49c8695deca15e1400
Tab-Y <tlyoumans@gmail.com>
2022-10-31 15:44:07 -0400

    base route page

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const notesRouter = require('./notes');     //imports route
+const app = express();
+
+// list for if there were more routes to other pages
+app.use('/notes', notesRouter);             //guides to notes router for direction
+
+module.exports = app;
