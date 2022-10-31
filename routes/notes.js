c7a8951785ff2d03b243e514ca51b15c711f9718
Tab-Y <tlyoumans@gmail.com>
2022-10-31 18:21:25 -0400

    finally linked routes correctly

--- routes/notes.js
+++ routes/notes.js
@@ -3,9 +3,27 @@ const { readFromFile, readAndAppend } = require('../helpers/fsUtils');          
 const uuid = require('../helpers/uuid');                                            // unique id
 
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));        //reads the database
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));        // reads the database
 });
 
-notes.post('/', (req, res) => {
-    
-})
+notes.post('/', (req, res) => {                                                // posts new notes
+    const { title, text } = req.body;
+    console.log(req.body);
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            tip_id: uuid(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        const response = {
+            status: "Note saved",
+            body: newNote,
+        };
+        res.json(response)
+    } else {
+        res.json('error occered')
+    };
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");                 // npm module being used for this project
 const path = require('path');                       // module for pathing 
+const routes = require('./routes/index');           // imports routing module
 const fsUtils = require('./helpers/fsUtils');       // core modules called into functions
 const uuid = require('./helpers/uuid');             // unique user id  -  a random generated id for use within the server
 const PORT = process.env.PORT || 3001;              // needed for server port in heroku or other server hosts
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', routes);
 
 app.use(express.static('public'));                  //used public folder for client side
 
@@ -16,6 +21,8 @@ app.get('/notes', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`listening at ${PORT}`));
+
+
 // GIVEN a note-taking application
 // WHEN I open the Note Taker               X
 // THEN I am presented with a landing page with a link to a notes page          X
