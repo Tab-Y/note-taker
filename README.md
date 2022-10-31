81fbfc91f787782b84f4a689d71fb14a7df394d3
Tab-Y <tlyoumans@gmail.com>
2022-10-31 15:35:21 -0400

    readme structure, update read/write function

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# note-taker
-an application for taking notes.
+# Note Taker 
+![License](https://img.shields.io/badge/License-mit-blue)
+
+## Description 
+
+will add later
+ 
+## Table of Contents
+ 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [Questions](#questions)
+- [Tests](#tests) 
+
+##  Installation 
+
+will add later 
+
+## Usage 
+
+will add later 
+
+![Descriptive text](assets/images/screenshot.png) 
+
+## Credits 
+
+n/a 
+
+## License 
+
+MIT License 
+Please refer to License in repository.  
+ 
+
+## Badges 
+
+Add badges to this section 
+
+## Features 
+
+Add features to this space 
+
+## Questions 
+
+If you have any questions or concerns, please reach out to Tab-Y at tlyoumans@gmail.com. 
+
+## Tests 
+
+test with jest 
+
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require("express");     // npm module being used for this project
-const path = require('path');           // module for pathing 
-const fs = require('fs');               // core module
-const uuid = require('./helpers/uuid');           // unique user id  -  a random generated id for use within the server
-const util = require('util');           // core module
-const PORT = process.env.PORT || 3001;  // needed for server port in heroku or other server hosts
+const express = require("express");                 // npm module being used for this project
+const path = require('path');                       // module for pathing 
+const fsUtils = require('./helpers/fsUtils');       // core modules called into functions
+const uuid = require('./helpers/uuid');             // unique user id  -  a random generated id for use within the server
+const PORT = process.env.PORT || 3001;              // needed for server port in heroku or other server hosts
 const app = express();
 
-app.use(express.static('public'));      //used public folder for client side
+app.use(express.static('public'));                  //used public folder for client side
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html')) //targets the public folder within this repo 
